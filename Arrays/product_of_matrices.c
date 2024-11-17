cff352d25de3c5f4c4247c7bd2295f6ca9e7d20d
5luck <lucky1008000@gmail.com>
2024-11-17 23:04:27 +0530

    2nd

---
+++ Arrays/product_of_matrices.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+     int mat[2][2],mat1[2][2],product[2][2];
+     for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("Enter the %d %d element of the first matrix : ",i+1,j+1);
+            scanf("%d",&mat[i][j]);
+        }
+     }
+     for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("Enter the %d %d element of the second matrix : ",i+1,j+1);
+            scanf("%d",&mat1[i][j]);
+        }
+     }
+     printf("First matrix : \n");
+     for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("\t%d",mat[i][j]);
+        }
+        printf("\n");
+     }
+      printf("Second matrix : \n");
+     for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("\t%d",mat1[i][j]);
+        }
+        printf("\n");
+     }
+     for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            for(int k = 0; k<2;k++){
+                
+            }
+        }
+        printf("\n");
+     }
+    return 0;
+}
