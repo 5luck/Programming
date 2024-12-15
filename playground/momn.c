cc3e0451c5f9eb6d4b20803230660052afbef53a
5luck <lucky1008000@gmail.com>
2024-12-15 22:39:19 +0530

    2nd

--- playground/momn.c
+++ playground/momn.c
@@ -4,13 +4,12 @@ int main()
      int m1[2][2],m2[2][2],m3[2][2]={0};
      for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            printf("\nEnter the %d %d element of the matrix: ",i,j);
-            scanf("%d",&m1[i][j]);
-        }
+            printf("\nEnter the %d %d element of the matrix: ",i+1,j+1);
+            scanf("%d",&m1[i][j]
      }
      for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            printf("\nEnter the %d %d element of the matrix: ",i,j);
+            printf("\nEnter the %d %d element of the matrix: ",i+1,j+1);
             scanf("%d",&m2[i][j]);
         }
      } 
