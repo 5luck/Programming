f3b5d140326f06d3db0d5718322fc3c2c84c00de
5luck <lucky1008000@gail.com>
2025-01-09 11:45:07 +0530

    hola

--- pointers/swap.c
+++ pointers/swap.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+void swap(int* a, int* b);
+void swap(int* a, int* b){
+    int temp=0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main()
 {
-     
+     int a;
+     int b;
+     printf("Enter the value of a & b: ");
+     scanf("%d %d",&a,&b);
+     swap(&a, &b);
+     printf("The swapped value of a is %d & b is %d", a, b);
     return 0;
 }
