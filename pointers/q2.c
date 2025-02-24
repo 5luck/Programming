39df6ec380cad60d35f9afb49b1f115106e15549
5luck <lucky1008000@gail.com>
2025-02-24 14:00:35 +0530

    initial commit

--- pointers/q2.c
+++ pointers/q2.c
@@ -1,8 +1,23 @@
-//Write a program having a variable Print the address ot Pass this variable
+//Write a program having a variable "i". Print the address of "i". Pass this variable
 //to a function and print its address. Are these addresses same? Why?
 #include <stdio.h>
-int main()
-{
-     
+
+// Function prototype
+void printAddress(int *ptr);
+
+int main() {
+    int i = 10;  // Declaring variable i
+    
+    // Printing address of i from main function
+    printf("Address of i in main(): %p\n", (void*)&i);
+    
+    // Passing address of i to function
+    printAddress(&i);
+    
     return 0;
+}
+
+void printAddress(int *ptr) {
+    // Printing address stored in ptr
+    printf("Address of i in printAddress(): %p\n", (void*)ptr);
 }
