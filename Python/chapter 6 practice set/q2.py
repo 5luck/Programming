0dd7713a6227bb69288b22c2b74291afd97fb72a
5luck <lucky1008000@gmail.com>
2025-07-03 12:06:16 +0530

    python

--- Python/chapter 6 practice set/q2.py
+++ Python/chapter 6 practice set/q2.py
@@ -4,3 +4,10 @@
 m1=int(input("Enter marks of subject 1: "))
 m2=int(input("Enter marks of subject 2: "))
 m3=int(input("Enter marks of subject 3: "))
+percentage=((m1+m2+m3)/300)*100
+if(m1>33 and m2>33 and m3>33 and percentage>40):
+    print("You are pass")
+    print(f"Your percentage: {percentage}")
+else:    
+    print("You failed")
+    print(f"Your percentage: {percentage}%")
