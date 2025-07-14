e634a083633fd2a8b5cbd856def295ab9cf7a685
5luck <lucky1008000@gmail.com>
2025-07-14 12:21:12 +0530

    file handling

--- Python/Projects/Project_1.py
+++ Python/Projects/Project_1.py
@@ -1,6 +1,4 @@
-# We all have played snake, water gun game in our childhood. If you haven’t, google the
-# rules of this game and write a python program capable of playing this game with the
-# user
+# We all have played snake, water gun game in our childhood. If you haven’t, google the rules of this game and write a python program capable of playing this game with the user
 import random
 game = ["Snake", "Water", "Gun"]
 lower_game = [item.lower() for item in game]
