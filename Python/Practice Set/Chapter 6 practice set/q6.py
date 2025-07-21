5a82ce3053c73ab01499e835c00e49e410f270c8
5luck <lucky1008000@gmail.com>
2025-07-21 11:53:03 +0530

    python

--- Python/Practice Set/Chapter 6 practice set/q6.py
+++ Python/Practice Set/Chapter 6 practice set/q6.py
@@ -12,22 +12,29 @@ m3=int(input("Enter marks of subject 3: "))
 m4=int(input("Enter marks of subject 4: "))
 m5=int(input("Enter marks of subject 5: "))
 m6=int(input("Enter marks of subject 6: "))
+
 percentage=(m1+m2+m3+m4+m5+m6)/600*100
+
 if(100>=percentage>90):
     print(f'''    Grade Ex
     Your percentage: {percentage}%''')
+
 elif(90>=percentage>=80):
     print(f'''    Grade A
     Your percentage: {percentage}%''')
+
 elif(80>=percentage>=70):
     print(f'''    Grade B
     Your percentage: {percentage}%''')
+
 elif(70>=percentage>=60):
     print(f'''    Grade C
     Your percentage: {percentage}%''')
+
 elif(60>=percentage>=50):
     print(f'''    Grade D
     Your percentage: {percentage}%''')
+    
 else:
     print(f'''    Grade F
     Your percentage: {percentage}%''')
